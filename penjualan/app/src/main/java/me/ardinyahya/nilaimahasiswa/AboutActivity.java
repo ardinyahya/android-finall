070376642b8ce28a28c814f49ec80d29f51bdeb4
ardinyahya <ardin.yahya@gmail.com>
2018-02-02 18:47:02 +0800

    push

---
+++ penjualan/app/src/main/java/me/ardinyahya/nilaimahasiswa/AboutActivity.java
@@ -0,0 +1,19 @@
+package me.ardinyahya.nilaimahasiswa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.arifwidi.nilaimahasiswa.R;
+
+/**
+ * Created by ardinyahya on 2/5/17.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
